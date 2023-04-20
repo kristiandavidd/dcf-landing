cb1663fd6f9f3b8a7a58e47d71ef6a7be8ba9d07
kristiandavidd <kristiandavid644@gmail.com>
2023-04-20 20:05:16 +0700

    add colors to tailwind.config.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
+          <h2 className={`${inter.className} mb-3 text-2xl font-semibold text-dcf-dark-brown`}>
             Docs{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
